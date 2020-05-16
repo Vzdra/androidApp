4dfae15659040b7f018f3e0c871acfb4b0171967
Vzdra <37705212+Vzdra@users.noreply.github.com>
2020-05-17 01:31:12 +0200

    updates

--- app/src/main/java/com/example/myappproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/myappproject/Activity/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.myappproject.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.myappproject.Fragment.HomeFragment;
 import com.example.myappproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        BottomNavigationView bottomNavView = findViewById(R.id.navigation_bar);
+        bottomNavView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
