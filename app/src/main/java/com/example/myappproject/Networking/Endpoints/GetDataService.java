685b5004738ed0c3cb23216b5e2eb85fdeb70c77
Vzdra <37705212+Vzdra@users.noreply.github.com>
2020-05-18 01:09:09 +0200

    added full response structure

--- app/src/main/java/com/example/myappproject/Networking/Endpoints/GetDataService.java
+++ app/src/main/java/com/example/myappproject/Networking/Endpoints/GetDataService.java
@@ -1,5 +1,6 @@
 package com.example.myappproject.Networking.Endpoints;
 
+import com.example.myappproject.ResponseStructures.Cities;
 import com.example.myappproject.ResponseStructures.Countries;
 import com.example.myappproject.ResponseStructures.DataObject;
 import com.example.myappproject.ResponseStructures.States;
@@ -12,12 +13,19 @@ import retrofit2.http.Path;
 
 public interface GetDataService {
 
-    @GET("/nearest_city?key={apikey}")
-    Call<DataObject> nearestCityResults(@Path("apikey") String ki);
+    @GET("/nearest_city?lat={lat}&lon={lon}key={apikey}")
+    Call<DataObject> nearestCityResults(@Path("apikey") String ki, @Path("lat") double lat, @Path("lon") double lon);
 
     @GET("/countries?key={apikey}")
     Call<Countries> supportedCountries(@Path("apikey") String ki);
 
     @GET("/states?country={country}&key={apikey}")
     Call<States> supportedStatesInCountry(@Path("apikey") String ki, @Path("country") String country);
+
+    @GET("/cities?state={state}&country={country}&key={apikey}")
+    Call<Cities> supportedCitiesInState(@Path("apikey") String ki, @Path("country") String country, @Path("state") String state);
+
+    @GET("/city?city={city}&state={state}&country={country}&key={apikey}")
+    Call<DataObject> cityInformation(@Path("apikey") String ki, @Path("country") String country, @Path("state") String state, @Path("city") String city);
+
 }
