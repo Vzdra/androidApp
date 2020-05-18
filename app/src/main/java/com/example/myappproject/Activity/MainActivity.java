e213e1aa7b5c28adb20d15b2e16ca62cc5dfd470
Vzdra <37705212+Vzdra@users.noreply.github.com>
2020-05-18 02:14:40 +0200

    added full response structure

--- app/src/main/java/com/example/myappproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/myappproject/Activity/MainActivity.java
@@ -6,10 +6,19 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.myappproject.Fragment.HomeFragment;
+import com.example.myappproject.Networking.Client.RetrofitClient;
+import com.example.myappproject.Networking.Endpoints.GetDataService;
 import com.example.myappproject.R;
+import com.example.myappproject.ResponseStructures.City;
+import com.example.myappproject.ResponseStructures.DataObject;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +28,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        HomeFragment currentFragment;
+        currentFragment = new HomeFragment();
+
         BottomNavigationView bottomNavView = findViewById(R.id.navigation_bar);
         bottomNavView.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
+        //currentFragment.setTextView("Hello playa!");
+
+        //=--------------------------------------
+//        GetDataService service = RetrofitClient.getRetrofitInstance().create(GetDataService.class);
+//        Call<DataObject> call = service.cityInformation("d0aad5b7-d828-4530-9b29-919746a39518", "North Macedonia", "Skopje", "Skopje");
+//        call.enqueue(new Callback<DataObject>() {
+//            @Override
+//            public void onResponse(Call<DataObject> call, Response<DataObject> response) {
+//                ((HomeFragment) currentFragment).setTextView(response.body().getState());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<DataObject> call, Throwable t) {
+//                System.out.println(t);
+//            }
+//        });
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+                    Fragment selectedFragment = new HomeFragment();
 
                     switch (item.getItemId()){
                         case R.id.navigation_home:
--- app/src/main/java/com/example/myappproject/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/myappproject/Fragment/HomeFragment.java
@@ -1,6 +1,30 @@
 package com.example.myappproject.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myappproject.R;
+
 public class HomeFragment extends Fragment {
+
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        textView = view.findViewById(R.id.textviewid);
+        return view;
+    }
+
+    public void setTextView(String text){
+        textView.setText(text);
+    }
 }
