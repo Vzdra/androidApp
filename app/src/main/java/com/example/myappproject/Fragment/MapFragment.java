a16cf9beb69b3e86866751f61874eae52166f81b
Vzdra <37705212+Vzdra@users.noreply.github.com>
2020-05-20 03:01:37 +0200

    fixed db error on install

--- app/src/main/java/com/example/myappproject/Fragment/MapFragment.java
+++ app/src/main/java/com/example/myappproject/Fragment/MapFragment.java
@@ -1,7 +1,17 @@
 package com.example.myappproject.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.myappproject.R;
 
 public class MapFragment extends Fragment {
 
+    
 }
